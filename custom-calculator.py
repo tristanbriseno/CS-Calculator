039e21570e04da9c9fa24fed52ae23d486756606
Tristan_Briseno <tristan.k.briseno@gmail.com>
2020-11-11 14:41:42 -0800

    first commit

---
+++ custom-calculator.py
@@ -0,0 +1,5 @@
+height = float(input("Input your height in meters: "))
+weight = float(input("Input your weight in kilogram: "))
+#this is where the code is requesting an input in the form of a float.
+print("Your body mass index is: ", round(weight / (height * height), 2))
+#Here is where the code is printing the text and the result of the conversion.
